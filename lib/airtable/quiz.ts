25ff1a479435b7fb59ced5e334468b846335fc79
Souvik <souvikde.ns@gmail.com>
2020-10-09 01:58:58 +0530

    Update quiz.ts

--- lib/airtable/quiz.ts
+++ lib/airtable/quiz.ts
@@ -8,4 +8,26 @@ export class AirtableDatabase {
         this.base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.QUIZZER_BASE);
     }
 
+    saveQuestion(question: string) {
+        this.base('quizzes').create([{
+            "fields": {
+                "Questions": question
+            }
+        }], (err, records) => {
+            if (err) {
+                throw err;
+            }
+            return records[0].getId();
+        })
+    }
+
+    retrieveQuestion(id: string) {
+        this.base('quizzes').find(id, (err, record) => {
+            if (err) {
+                throw err;
+            }
+            return record
+        })
+    }
+
 }
