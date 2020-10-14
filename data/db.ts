a5a85fd9deebf508b05a05de9cdb0af2a190374d
Souvik <souvikde.ns@gmail.com>
2020-10-14 20:23:46 +0530

    creating db class

--- data/db.ts
+++ data/db.ts
@@ -1,4 +1,11 @@
 import fs from 'fs';
+import uniqid from 'uniqid'
+
+interface StoreType {
+    id: string,
+    question: string,
+    answers: string
+}
 
 export default class Db {
     private filePath: string;
@@ -7,8 +14,40 @@ export default class Db {
     }
 
     addQuiz(question: string) {
+        let id = uniqid();
+        let data = this._fetchData();
+        let newQuestion: StoreType = {
+            id: id,
+            question: question,
+            answers: ''
+        }
+        data.push(newQuestion);
+
+        this._write(data);
+
+    }
+    fetchAQuiz(id: string): StoreType {
+        let data: Array<StoreType> = this._fetchData();
+
+        let res: StoreType;
+
+        data.forEach((el: StoreType) => {
+            if(el.id === id){
+                res = el;
+            }
+        });
+
+        return res;
+    }
+
+    private _fetchData(): Array<StoreType> {
         let data = fs.readFileSync(this.filePath);
-        console.log(JSON.parse(data.toString()))
-        fs.writeFileSync(this.filePath, question);
+        let db: Array<StoreType> = JSON.parse(data.toString());
+
+        return db
+    }
+
+    private _write(data: Array<StoreType>) {
+        fs.writeFileSync(this.filePath, JSON.stringify(data))
     }
 }
