8f677e722b771f014c372e888f80f28e1bc5780c
Souvik <souvikde.ns@gmail.com>
2020-10-15 23:30:08 +0530

    Update create.tsx

--- pages/create.tsx
+++ pages/create.tsx
@@ -94,6 +94,7 @@ const Create = () => {
                                 setDialog(!dialog)
                             }}
                         >
+                            
                             Create Quiz
                         </Button>
                     </Grid>
