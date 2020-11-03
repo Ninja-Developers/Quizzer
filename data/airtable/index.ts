406591d4b344b128abc907e699cfedfca7b38241
Souvik <souvikde.ns@gmail.com>
2020-11-03 15:12:18 +0530

    Update index.ts

--- data/airtable/index.ts
+++ data/airtable/index.ts
@@ -1,12 +1,21 @@
 import Axios from 'axios'
 export class Airtable{
-    private URL: string = "ttps://api.airtable.com/v0";
+    private URL: string = `https://api.airtable.com/v0/${process.env.QUIZZER_BASE}`;
     constructor(){
     }
     async fetchAQuiz(id: string){
-        Axios({
-            url: this.URL,
+        try {
+            let data = await Axios({
+                url: this.URL + `/quizzes`,
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+                }
+            })
 
-        })
+            return data.data
+        } catch (error) {
+            throw error
+        }
     }
 }
