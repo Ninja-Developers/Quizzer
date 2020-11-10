d8f9e21e9f04cad4a850784ac8aea126f9d7cdb2
Souvik <souvikde.ns@gmail.com>
2020-11-11 02:14:50 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import * as express from 'express';
 import {PORT} from './config';
+import * as path from 'path';
+
 
 // importing api routes 
 import apiRoutes from './router';
 
 const app = express();
+app.use(express.raw());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.resolve(__dirname, '../', 'client', 'build')))
 
 app.use('/api', apiRoutes);
+
+// client handling 
+app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
+})
 
 
 app.listen(PORT, ()=> {
