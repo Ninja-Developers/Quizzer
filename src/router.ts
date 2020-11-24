a82b08acc0ad929bd56ce5df8d161e6908ccdb3b
Souvik <souvikde.ns@gmail.com>
2020-11-24 23:16:41 +0530

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -2,5 +2,10 @@ import {Router} from 'express';
 
 const router =  Router();
 
+router.route('/')
+    .get((req,res) => {
+        res.status(200).send('Quizzer API ');
+    })
+
 
 export default router;
