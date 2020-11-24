956870d24019877104b12216f0edae5bc0abd925
Souvik <souvikde.ns@gmail.com>
2020-11-24 23:24:18 +0530

    connect mongoose and installed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
-import {PORT} from './config';
+import { PORT, MONGO_URL } from './config';
 import * as path from 'path';
-
+import * as mongoose from 'mongoose';
 
 // importing api routes 
 import apiRoutes from './router';
@@ -9,18 +9,22 @@ import apiRoutes from './router';
 const app = express();
 app.use(express.raw());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, '../', 'client', 'build')))
 
 app.use('/api', apiRoutes);
 
 // client handling 
-app.get('*',(req,res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'))
 })
 
+mongoose.connect(MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        console.log('Database Connected')
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        })
+    })
 
-app.listen(PORT, ()=> {
-    console.log(`Server started on port ${PORT}`);
-})
