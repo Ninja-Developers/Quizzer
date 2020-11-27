b92ae1eac4e21c00d87eeec6a87e4b7c21d016f3
Souvik <souvikde.ns@gmail.com>
2020-11-27 22:31:11 +0530

    created basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { io } from 'socket.io-client'
+import { io } from 'socket.io-client';
+import { Route, Switch } from 'react-router-dom';
+
+// importing pages 
+import Home from './pages/home'
+import Room from './pages/room'
 
 let socket;
 
 function App() {
   React.useEffect(() => {
     socket = io('http://localhost:5000/');
-    socket.emit('join', {name: 'Souvik'});
+    socket.emit('join', { name: 'Souvik' });
     console.log('souvik')
   }, [])
   return (
     <div>
-      <h1>
-        Quizzer App
-      </h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/room" exact component={Room} />
+      </Switch>
     </div>
   );
 }
