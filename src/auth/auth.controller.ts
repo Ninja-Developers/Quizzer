14ac57738b986c483cb6b0fe6f7347fa9c32b3e3
Souvik <souvikde.ns@gmail.com>
2020-12-10 01:45:48 +0530

    creating controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express'
 
 export const localLogin = (req: Request, res: Response) => {
-    
+
+}
+
+export const localSignup = (req: Request, res: Response) => {
+
 }
