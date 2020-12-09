2c3116a0b49386f421fc5dac1ad9cbb3cf2baf86
Souvik <souvikde.ns@gmail.com>
2020-12-10 03:24:35 +0530

    created signup controller logic

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,6 +1,7 @@
 import { User } from './user';
 import { userModel } from './user.model';
 import { CreateUserRequest } from './types'
+import * as bcrypt from 'bcrypt';
 
 export class UserDao {
     constructor() {
@@ -29,11 +30,19 @@ export class UserDao {
          * this is where we will hash our password 
          * before saving it into the database. 
          */
+
+        let {
+            username,
+            password,
+            email
+        } = req;
         try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+
             let res: any = await userModel.create({
-                username: req.username,
-                password: req.password,
-                email: req.email
+                username: username,
+                password: hashedPassword,
+                email: email
             });
 
             let user = new User(res.username, res.password);
