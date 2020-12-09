6cd17d1677885a535b6a66aaaf301440ce36dd53
Souvik <souvikde.ns@gmail.com>
2020-12-10 01:48:55 +0530

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,23 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import { UserDao } from '../user';
+const userDao = new UserDao();
 
 export const localLogin = (req: Request, res: Response) => {
 
 }
 
-export const localSignup = (req: Request, res: Response) => {
+export const localSignup = async (req: Request, res: Response) => {
+    let { username, password } = req.body;
 
+    try {
+        let user = await userDao.create({
+            username,
+            password
+        });
+
+        return res.status(201).json(user);
+        
+    } catch (error) {
+        return res.status(404).send(error);
+    }
 }
