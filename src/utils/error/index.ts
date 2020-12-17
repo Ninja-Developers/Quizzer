5eecad8a0b067874f6e14bb12fa75c5ddc881e1b
Souvik <souvikde.ns@gmail.com>
2020-12-17 19:28:49 +0530

    Update index.ts

--- src/utils/error/index.ts
+++ src/utils/error/index.ts
@@ -1,7 +1,6 @@
 import { HttpError } from './error';
 import { Response } from 'express';
 
-
 export const errorHandler = (res: Response, error: HttpError) => {
     if (!(error instanceof HttpError)) {
         return res.status(500).send("internal Server Error")
