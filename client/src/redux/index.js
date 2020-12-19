5ef3bd52aaf56167bb9096d3ad0d22304f15aa6b
Souvik <souvikde.ns@gmail.com>
2020-12-20 03:05:44 +0530

    connected redux

---
+++ client/src/redux/index.js
@@ -0,0 +1,13 @@
+import {
+    combineReducers,
+    createStore,
+    applyMiddleware
+} from 'redux';
+
+import thunk from 'redux-thunk';
+
+const store = createStore(combineReducers({
+
+}), applyMiddleware(thunk));
+
+export default store;
